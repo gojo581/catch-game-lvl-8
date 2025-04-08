4d770e29f070ffc5dd80e7fedd2256af9ce4fb2a
gojo581 <gadi17levin@gmail.com>
2025-04-08 15:29:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function addpoints(){
 
 point=point+100
 coin.innerHTML=point
-if(point==17000){alert('i see.. you are very good good job but you need to go the the next lvl(10)')|| alert('you are tp for the next lvl...')||alert('and now... you are in the final lvl good luck')||window.location.replace('https://gojo581.github.io/catch-game-lvl-9/')}
+if(point==17000){alert('i see.. you are very good good job but you need to go the the next lvl(9)')|| alert('you are tp for the next lvl...')||alert('and now... you are in the final lvl good luck')||window.location.replace('https://gojo581.github.io/catch-game-lvl-9/')}
 }
