3761bc216c0067222d45d79fca5a2e0b24ef9eef
gojo581 <gadi17levin@gmail.com>
2025-04-08 15:08:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let speed=2
+let point=document.getElementById('coin')
+function addpoints(){
+
+point=point+100
+coin.innerHTML=point
+if(point==17000){alert('i see.. you are very good good job but you need to go the the next lvl(10)')|| alert('you are tp for the next lvl...')||alert('and now... you are in the final lvl good luck')||window.location.replace('file:///C:/Users/gadi1/OneDrive/%D7%A9%D7%95%D7%9C%D7%97%D7%9F%20%D7%94%D7%A2%D7%91%D7%95%D7%93%D7%94/catch%20game%2010/index.html')}
+}
